4315558ce568bd76b08976204e0afa962ccb9848
laxmanprometteur <laxman.n@prometteur.in>
2023-07-02 00:23:21 +0530

    added image section

--- src/Pages/ImagesSection.jsx
+++ src/Pages/ImagesSection.jsx
@@ -13,63 +13,110 @@ const ImagesSection = () => {
             </p>
             <div className="container">
               <div className="row">
-                <div className="col-lg-2 col-md-3 col-sm-6">
+                <div className="col-lg-2 col-md-3 col-sm-6 mx-1 my-2">
                   <div class="WE2ijA">
                     <img
                       alt=""
                       src="https://template.canva.com/EAD7RS_AUHA/5/0/400w-7xP3yZCn8fI.jpg"
-                      className="im1gjQ w-100"
+                      className="im1gjQ-image  w-100"
+                      height={200}
                       draggable="false"
                     />
                   </div>
                 </div>
-                <div className="col-lg-2 col-md-3 col-sm-6">
+                <div className="col-lg-2 col-md-3 col-sm-6 mx-1 my-2">
                   <div class="WE2ijA">
                     <img
                       alt=""
-                      src="https://template.canva.com/EAD7RS_AUHA/5/0/400w-7xP3yZCn8fI.jpg"
-                      className="im1gjQ w-100"
+                      src="https://template.canva.com/EADapG51fls/3/0/400w-aChlnr5L_Ww.jpg"
+                      className="im1gjQ-image w-100"
                       draggable="false"
                     />
                   </div>
                 </div>
 
-                <div className="col-lg-2 col-md-3 col-sm-6">
+                <div className="col-lg-2 col-md-3 col-sm-6 mx-1 my-2">
                   <div class="WE2ijA">
                     <img
                       alt=""
-                      src="https://template.canva.com/EAD7RS_AUHA/5/0/400w-7xP3yZCn8fI.jpg"
-                      className="im1gjQ w-100"
+                      src="https://template.canva.com/EAEXA859wm0/2/0/400w-jRAxCD5Qpy4.jpg"
+                      className="im1gjQ-image w-100"
+                      height={200}
                       draggable="false"
                     />
                   </div>
                 </div>
-                <div className="col-lg-2 col-md-3 col-sm-6">
+                <div className="col-lg-2 col-md-3 col-sm-6 mx-1 my-2">
                   <div class="WE2ijA">
                     <img
                       alt=""
-                      src="https://template.canva.com/EAD7RS_AUHA/5/0/400w-7xP3yZCn8fI.jpg"
-                      className="im1gjQ w-100"
+                      src="https://template.canva.com/EAEgGRQ-LHs/7/0/400w-zHUy6GwfcuI.jpg"
+                      className="im1gjQ-image w-100"
                       draggable="false"
                     />
                   </div>
                 </div>
-                <div className="col-lg-2 col-md-3 col-sm-6">
+                <div className="col-lg-2 col-md-3 col-sm-6 mx-1 my-2">
                   <div class="WE2ijA w-100">
                     <img
                       alt=""
-                      src="https://template.canva.com/EAD7RS_AUHA/5/0/400w-7xP3yZCn8fI.jpg"
-                      className="im1gjQ w-100"
+                      src="https://template.canva.com/EADao85M6_0/1/0/400w-4vwp1100Ykw.jpg"
+                      className="im1gjQ-image w-100"
+                      height={200}
                       draggable="false"
                     />
                   </div>
                 </div>
-                <div className="col-lg-2 col-md-3 col-sm-6">
+                <div className="col-lg-2 col-md-3 col-sm-6 mx-1 my-2">
                   <div class="WE2ijA">
                     <img
                       alt=""
-                      src="https://template.canva.com/EAD7RS_AUHA/5/0/400w-7xP3yZCn8fI.jpg"
-                      className="im1gjQ w-100"
+                      src="https://template.canva.com/EADaosVPiEU/1/0/400w-HI9ygtHJxXI.jpg"
+                      className="im1gjQ-image w-100"
+                      draggable="false"
+                    />
+                  </div>
+                </div>
+
+                <div className="col-lg-2 col-md-3 col-sm-6 mx-1 my-2">
+                  <div class="WE2ijA">
+                    <img
+                      alt=""
+                      src="https://template.canva.com/EAEgGRQ-LHs/7/0/400w-zHUy6GwfcuI.jpg"
+                      className="im1gjQ-image w-100"
+                      draggable="false"
+                      height={200}
+                    />
+                  </div>
+                </div>
+
+                <div className="col-lg-2 col-md-3 col-sm-6 mx-1 my-2">
+                  <div class="WE2ijA">
+                    <img
+                      alt=""
+                      src="https://template.canva.com/EADakBMT2L8/7/0/400w-wHZFkdNCb4g.jpg"
+                      className="im1gjQ-image w-100"
+                      draggable="false"
+                    />
+                  </div>
+                </div>
+                <div className="col-lg-2 col-md-3 col-sm-6 mx-1 my-2">
+                  <div class="WE2ijA">
+                    <img
+                      alt=""
+                      src="https://template.canva.com/EAEgGRQ-LHs/7/0/400w-zHUy6GwfcuI.jpg"
+                      className="im1gjQ-image w-100"
+                      draggable="false"
+                      height={200}
+                    />
+                  </div>
+                </div>
+                <div className="col-lg-2 col-md-3 col-sm-6 mx-1 my-2">
+                  <div class="WE2ijA">
+                    <img
+                      alt=""
+                      src="https://content-management-files.canva.com/35b5c343-4194-4ae8-90f1-7ec803d4600d/template-4.jpg"
+                      className="im1gjQ-image w-100"
                       draggable="false"
                     />
                   </div>
